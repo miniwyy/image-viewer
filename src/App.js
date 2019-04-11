7ea5dd291f8ceb508a274fe78a2ec381119b1615
yongyu <594373079@qq.com>
2019-04-11 18:16:42 +0800

    :hammer: refactor code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import SingleImgView from '../src/ImageViewer/index.js'
+import SingleImgView from './components/index.js';
 
 class App extends Component {
   state = {
@@ -7,31 +7,42 @@ class App extends Component {
     current: 1
   }
   show(current) {
-      this.setState({
-        showViewer: true,
-        current
-      })
+    this.setState({
+      showViewer: true,
+      current
+    })
   }
   close() {
-      this.setState({ showViewer: false})
+    this.setState({
+      showViewer: false
+    })
   }
   render() {
     const { showViewer, current } = this.state;
     let imagelist = [
-            'https://zmage.caldis.me/imgSet/childsDream/1.jpg',
-            'https://zmage.caldis.me/imgSet/childsDream/2.jpg',
-            'https://zmage.caldis.me/imgSet/childsDream/3.jpg'
-        ];
+      'https://zmage.caldis.me/imgSet/childsDream/1.jpg',
+      'https://zmage.caldis.me/imgSet/childsDream/2.jpg',
+      'https://zmage.caldis.me/imgSet/childsDream/3.jpg'
+    ];
     return (
       <div>
-          <ul className="gallery">
-            { imagelist.map((item, i)=>{
-                return (<li key={i}><img className="pic" src={item} onClick={this.show.bind(this, i)} alt="" /></li>)
-            })}
-          </ul>  
-          {
-            !!showViewer && <SingleImgView current={current} imagelist={imagelist} close={this.close.bind(this)} />
-          }
+        <ul className="gallery">
+          {imagelist.map((item, i)=>{
+            return (
+              <li key={i}>
+                <img
+                  className="pic"
+                  src={item}
+                  onClick={this.show.bind(this, i)}
+                  alt=""
+                />
+              </li>
+            )
+          })}
+        </ul>  
+        {
+          !!showViewer && <SingleImgView current={current} imagelist={imagelist} close={this.close.bind(this)} />
+        }
       </div>
     );
   }
